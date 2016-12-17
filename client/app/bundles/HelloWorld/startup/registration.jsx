066b4259cbc7b07db5d1a4d3e00b21a0663baea7
EEtancelin <etancelin.edouard@gmail.com>
2016-12-17 22:15:00 +0100

    Change the app name from Helloworld to Elephant Snipp

--- client/app/bundles/HelloWorld/startup/registration.jsx
+++ client/app/bundles/HelloWorld/startup/registration.jsx
@@ -1,8 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorldApp from './HelloWorldApp';
+import ElephantSnippApp from './ElephantSnippApp';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorldApp,
+  ElephantSnippApp,
 });
