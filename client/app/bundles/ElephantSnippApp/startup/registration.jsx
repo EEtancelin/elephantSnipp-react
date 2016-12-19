8432aaa59c71460de75346fd89c845955309da63
EEtancelin <etancelin.edouard@gmail.com>
2016-12-19 17:05:08 +0100

    Save Supression

--- client/app/bundles/ElephantSnippApp/startup/registration.jsx
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import ElephantSnippApp from './ElephantSnippApp';
-
-// This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-  ElephantSnippApp,
-});
